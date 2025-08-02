b536ded1fb6271f3c30d1216c91a2b619b8f921d
vle <vuongle6@gmail.com>
2025-08-02 18:39:07 -0400

    add dust

--- scripts/hammy_and_wheel_tester.gd
+++ scripts/hammy_and_wheel_tester.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
+var dust_threshold = 2.0
 @export_range(1.0, 5.0) var speed : float = 1.0
+
 
 func update_speed(new_speed) -> void:
 	$wheel/AnimationPlayer.speed_scale = new_speed
 	$hammy_run/AnimationPlayer.speed_scale = new_speed
+	if new_speed > dust_threshold:
+		$SmokeNDustP03Vfx1.visible = true
+	else:
+		$SmokeNDustP03Vfx1.visible = false
 
 func _process(delta: float):
 	update_speed(speed)
