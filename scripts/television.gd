df098a3b298ddc7f5de99d64783340a9925c44e0
James Kingdon <kingdon909@gmail.com>
2025-08-03 02:57:22 -0400

    tv sounds

--- scripts/television.gd
+++ scripts/television.gd
@@ -1,17 +1,19 @@
 extends Node2D
 
-func _ready():
-	_turn_on()
-
 func _turn_on():
 	$Noise/AnimationPlayer.play("on")
+	$"tv-on".play()
 
 func _shut_down():
 	$Noise/AnimationPlayer.play("off")
+	$"tv-off".play()
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "on":
 		$Noise/AnimationPlayer.play("static")
+
+func _on_timer_timeout() -> void:
+	_turn_on()
 
 func _on_game_over() -> void:
 	_shut_down()
