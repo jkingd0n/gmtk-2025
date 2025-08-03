d576cb441eb7681d346c82f738d217c299f9bd9e
James Kingdon <kingdon909@gmail.com>
2025-08-03 13:26:06 -0400

    lamp stuff

--- scripts/lamp.gd
+++ scripts/lamp.gd
@@ -1,12 +1,23 @@
 extends Node2D
 
+var is_game_over : bool = false
+var is_on : bool = false
+
 func _turn_on():
+	if is_game_over:
+		return
+	is_on = true
 	$light.show()
 	$lamp.frame = 1
+	$on.play()
 	
 func _turn_off():
+	is_game_over = true
+	if not is_on:
+		return
 	$light.hide()
 	$lamp.frame = 0
+	$off.play()
 
 func _on_timer_timeout():
 	_turn_on()
